c611ea2c2fb39abe51e2e6afff1d892ec69b84f2
masterjindal <jindal08mayank@gmail.com>
2024-02-26 22:08:46 +0530

    lessAdded

--- CompanyEmployee/App_Start/BundleConfig.cs
+++ CompanyEmployee/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace CompanyEmployee
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
         }
     }
 }
